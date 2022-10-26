7ff2849324c39a24d86523f29b66754e8ec6490c
Andri Yabu Ndapa Rehing <andriyabu@gmail.com>
2022-10-26 15:50:54 +0700

    added asets img and video, created index.css, data.js as data API, create Hero style

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
-const Hero = () => {
+const Hero = ({heroapi: {title, subtitle, btntext, img, sociallinks, videos}}) => {
+  // console.log(heroapi)
   return (
-    <div>Hero</div>
+    <>
+      <div className='relative h-auto w-auto flex flex-col'>
+        <div className='bg-theme clip-path h-[85vh] lg:h-75[vh] md:h-[65vh] sm:h-[55vh] w-auto absolute top-0 left-0 right-0 opacity-100 z-10'></div>
+        <div className='relative opacity-100 z-20 grid items-center justify-items-center nike-container'>
+          <div className='grid items-center justify-items-center mt-28 md:mt-24'>
+            <h1>{title}</h1>
+            <h1>{subtitle}</h1>
+            <button>{btntext}</button>
+            <div></div>
+            <div></div>
+          </div>
+          <div>
+            <img src={img} alt='hero-img/img' className=''/>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
