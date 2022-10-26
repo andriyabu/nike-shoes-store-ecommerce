590e48f62efd7f1e2293aab316f166095b870dd7
Andri Yabu Ndapa Rehing <andriyabu@gmail.com>
2022-10-26 16:07:15 +0700

    created index.js to export all assets components and import it from src/data/data.js

---
+++ src/assets/index.js
@@ -0,0 +1,33 @@
+export { default as heroimg} from "./hero.png";
+
+export { default as hightlightimg} from "./hightlightimg.png";
+export { default as sneakershoe} from "./sneaker.png";
+
+export { default as clip} from "./video/clip.mp4";
+export { default as vcover1} from "./video/vcover1.png";
+export { default as vcover2} from "./video/vcover2.png";
+export { default as vcover3} from "./video/vcover3.png";
+
+export { default as psale1} from "./nike-air-red.png";
+export { default as psale2} from "./nike-adapt-bb.png";
+export { default as psale3} from "./nike-adapt-bb-smart.png";
+
+export { default as product1} from "./product1.png";
+export { default as product2} from "./product2.png";
+export { default as product3} from "./product3.png";
+export { default as product4} from "./product4.png";
+export { default as product5} from "./product5.png";
+export { default as product6} from "./product6.png";
+export { default as product7} from "./product7.png";
+export { default as product8} from "./product8.png";
+export { default as product9} from "./product9.png";
+export { default as product10} from "./product10.png";
+export { default as product11} from "./product11.png";
+export { default as product12} from "./product12.png";
+
+
+export { default as facebook} from "./facebook.svg";
+export { default as instagram} from "./instagram.svg";
+export { default as twitter} from "./twitter.svg";
+export { default as youtube} from "./youtube.svg";
+export { default as messenger} from "./messenger.svg";
--- src/data/data.js
+++ src/data/data.js
@@ -1,36 +1,4 @@
-import heroimg from "../assets/hero.png";
-
-import hightlightimg from "../assets/hightlightimg.png";
-import sneakershoe from "../assets/sneaker.png";
-
-import clip from "../assets/video/clip.mp4";
-import vcover1 from "../assets/video/vcover1.png";
-import vcover2 from "../assets/video/vcover2.png";
-import vcover3 from "../assets/video/vcover3.png";
-
-import psale1 from "../assets/nike-air-red.png";
-import psale2 from "../assets/nike-adapt-bb.png";
-import psale3 from "../assets/nike-adapt-bb-smart.png";
-
-import product1 from "../assets/product1.png";
-import product2 from "../assets/product2.png";
-import product3 from "../assets/product3.png";
-import product4 from "../assets/product4.png";
-import product5 from "../assets/product5.png";
-import product6 from "../assets/product6.png";
-import product7 from "../assets/product7.png";
-import product8 from "../assets/product8.png";
-import product9 from "../assets/product9.png";
-import product10 from "../assets/product10.png";
-import product11 from "../assets/product11.png";
-import product12 from "../assets/product12.png";
-
-
-import facebook from "../assets/facebook.svg";
-import instagram from "../assets/instagram.svg";
-import twitter from "../assets/twitter.svg";
-import youtube from "../assets/youtube.svg";
-import messenger from "../assets/messenger.svg";
+import { heroimg, hightlightimg, sneakershoe, clip, vcover1, vcover2, vcover3, psale1, psale2, psale3, product1, product2, product3, product4, product5, product6, product7, product8, product9, product10, product11, product12, facebook, instagram, twitter, youtube, messenger} from '../assets'
 
 const heroapi = {
   title: "Play With Electric Nike",
